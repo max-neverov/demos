4955647b241c46006b373608ec13695acdab895d
Max Neverov <neverov.max@gmail.com>
2020-03-19 23:04:00 +0100

    Internal package implements one of the interfaces

--- incompatible-feature/nonewfeatures/nonewfeatures.go
+++ incompatible-feature/nonewfeatures/nonewfeatures.go
@@ -1,9 +1,15 @@
 package nonewfeatures
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Foo struct{}
 
 func (f Foo) Bar() {
-	fmt.Println("no new features in internal package")
+	fmt.Println("no new features in another package")
+}
+
+func (f Foo) Foo() {
+	fmt.Println("implement one of the interfaces in another package")
 }
